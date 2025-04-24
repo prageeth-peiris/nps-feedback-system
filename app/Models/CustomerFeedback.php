850e6ed4d0e77cd5b07dc7583912e685786be9b8
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-24 21:32:58 +0530

    linting

--- app/Models/CustomerFeedback.php
+++ app/Models/CustomerFeedback.php
@@ -6,11 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class CustomerFeedback extends Model
 {
-
-
-protected $fillable = ['feedback_score','answer_to_follow_up_question','response_group'];
-
-
-
-   
+    protected $fillable = ['feedback_score', 'answer_to_follow_up_question', 'response_group'];
 }
