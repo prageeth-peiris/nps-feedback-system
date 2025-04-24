07e0f6d599536b41f6c6f12ee034b0ecfb9494d1
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-24 23:42:43 +0530

    defined a method to get response group checking score

--- app/DTO/CustomerFeedbackDTO.php
+++ app/DTO/CustomerFeedbackDTO.php
@@ -4,12 +4,34 @@ namespace App\DTO;
 
 class CustomerFeedbackDTO extends BaseDataTransferObject {
 
-
+// uses constructor property promotion
 public function __construct(
     public int $feedback_score,
-    public string $answer_to_follow_up_question,
-    public string  $response_group
+    public string $answer_to_follow_up_question
 ){
+}
+
+
+public function getResponseGroup():string{
+
+    $score = $this->feedback_score;
+
+    if($score >= 9 && $score <= 10){
+            return "Promoter";
+    }
+
+    if($score >= 7 && $score <= 8){
+        return "Passive";
+}
+
+if($score >= 0 && $score <= 6){
+    return "Passive";
+}
+
+
+throw new \Exception("Invalid Feedback score to define the response group");
+
+
 }
 
 
