14e52832193369abd9b16a81de0df74acea95579
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-24 21:04:54 +0530

    created CustomerFeedback model and db-migration

---
+++ app/Models/CustomerFeedback.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerFeedback extends Model
+{
+
+
+protected $fillable = ['feedback_score','answer_to_follow_up_question','response_group'];
+
+
+
+   
+}
---
+++ database/migrations/2025_04_24_150554_create_customer_feedback_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_feedback', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('feedback_score');
+            $table->text('answer_to_follow_up_question')->nullable(); //this can be a large answer from customer so text can hold 65 000 chars
+            $table->char('response_group',50);  //  response group  can be promoters , passives , detractors according to feedback score
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_feedback');
+    }
+};
