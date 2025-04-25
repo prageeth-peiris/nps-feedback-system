d8702787c1edff44c5ecd847038a7e4e849c56da
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-25 21:42:01 +0530

    created general Data Transfer Object that can be used for pagination , sorting and filtering for data tables

---
+++ app/DTO/DataTableDTO.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DTO;
+
+
+// here we defined a simple Data Transfer Object to handle pagination of data retrieval
+class DataTableDTO extends BaseDataTransferObject
+{
+
+
+    public function __construct(
+        public readonly string $sortingColumn = 'id' ,
+        public readonly string $sortingDirection  = 'asc',
+
+//        we can pass where clauses here like this. ex :
+//         [
+//
+//             ["type","=","Passive"],
+//             ["feedback_score",">",5]
+//
+//
+//             ]
+        public readonly array $filters = [] ,
+
+
+        public readonly int $offset = 0,
+        public readonly int $limit = 10,
+    )
+    {
+    }
+}
