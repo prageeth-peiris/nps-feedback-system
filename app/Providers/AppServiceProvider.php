b261de2721c497931fe61b7a2dddec1721e7fab5
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-26 00:16:50 +0530

    bounded  CustomerFeedBackService contract to its  implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\CustomerFeedback\CustomerFeedbackRepositoryContract;
 use App\Repositories\CustomerFeedback\CustomerFeedBackRepositoryImplementation;
+use App\Services\CustomerFeedback\CustomerFeedbackServiceContract;
+use App\Services\CustomerFeedback\CustomerFeedbackServiceImplementation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
 
        $this->app->bind(CustomerFeedbackRepositoryContract::class,CustomerFeedBackRepositoryImplementation::class);
+       $this->app->bind(CustomerFeedBackServiceContract::class,CustomerFeedBackServiceImplementation::class);
 
 
     }
