629e3d1e28313813dfe4f13fd35fe4a72ce50841
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-26 00:16:29 +0530

    created CustomerFeedBackService implementation

---
+++ app/Services/CustomerFeedback/CustomerFeedbackServiceImplementation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\CustomerFeedback;
+
+use App\DTO\CustomerFeedbackDTO;
+use App\DTO\DataTableDTO;
+use App\Repositories\CustomerFeedback\CustomerFeedbackRepositoryContract;
+use Illuminate\Support\Collection;
+
+class CustomerFeedbackServiceImplementation implements CustomerFeedbackServiceContract
+{
+
+
+    public function __construct(
+        private readonly CustomerFeedbackRepositoryContract $customerFeedbackRepository
+    )
+    {
+    }
+
+    public function save(CustomerFeedbackDTO $customerFeedbackDTO): void
+    {
+            $this->customerFeedbackRepository->save($customerFeedbackDTO);
+
+    }
+
+    public function retrieve(DataTableDTO $dataTableDTO):Collection
+    {
+        return $this->customerFeedbackRepository->retrieve($dataTableDTO);
+    }
+
+
+}
