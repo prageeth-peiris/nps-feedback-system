ce9a3653ded615dd3fc99b4ecef9f606d63f9b1d
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-25 23:34:57 +0530

    implemented  retrieve method for the customer feedback repository

--- app/Repositories/CustomerFeedback/CustomerFeedBackRepositoryImplementation.php
+++ app/Repositories/CustomerFeedback/CustomerFeedBackRepositoryImplementation.php
@@ -3,7 +3,9 @@
 namespace App\Repositories\CustomerFeedback;
 
 use App\DTO\CustomerFeedbackDTO;
+use App\DTO\DataTableDTO;
 use App\Models\CustomerFeedback;
+use Illuminate\Support\Collection;
 
 class CustomerFeedBackRepositoryImplementation implements CustomerFeedbackRepositoryContract
 {
@@ -12,6 +14,21 @@ class CustomerFeedBackRepositoryImplementation implements CustomerFeedbackReposi
 
         CustomerFeedback::create($customerFeedbackDTO->toArray());
 
+
+
+    }
+
+    public function retrieve(DataTableDTO $dataTableDTO): Collection
+    {
+
+
+        return CustomerFeedback::query()
+               ->where($dataTableDTO->filters)
+            ->orderBy($dataTableDTO->sortingColumn,$dataTableDTO->sortingDirection)
+            ->limit($dataTableDTO->limit)
+            ->offset($dataTableDTO->offset)
+            ->get()
+            ;
 
 
     }
