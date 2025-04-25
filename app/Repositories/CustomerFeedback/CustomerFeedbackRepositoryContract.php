fffcbc096416008976d1ed2e7b8c3e9f7e592480
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-25 23:32:55 +0530

    added retrieve method for the customer feedback repository contract

--- app/Repositories/CustomerFeedback/CustomerFeedbackRepositoryContract.php
+++ app/Repositories/CustomerFeedback/CustomerFeedbackRepositoryContract.php
@@ -3,6 +3,8 @@
 namespace App\Repositories\CustomerFeedback;
 
 use App\DTO\CustomerFeedbackDTO;
+use App\DTO\DataTableDTO;
+use Illuminate\Support\Collection;
 
 interface  CustomerFeedbackRepositoryContract
 {
@@ -10,6 +12,6 @@ interface  CustomerFeedbackRepositoryContract
 
     public function save(CustomerFeedbackDTO $customerFeedbackDTO);
 
-
+    public function retrieve(DataTableDTO $dataTableDTO):Collection;
 
 }
