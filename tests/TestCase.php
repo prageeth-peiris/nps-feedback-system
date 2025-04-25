e783b462f378132b32fdc8883df3f51b4b1aef10
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-25 20:44:47 +0530

    added trait of RefreshDatabase to recreate  database fresh  before each test runs

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+
+    // clear the database before run any of test. that way test is not dependant of previous states
+    use RefreshDatabase;
+
 }
