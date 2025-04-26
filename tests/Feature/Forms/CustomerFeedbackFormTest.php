c8bb865ad7f3d6c95d4a07f26d4b05a1021993d1
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-26 18:10:51 +0530

    created test to for store method of CustomerFeedbackController

---
+++ tests/Feature/Forms/CustomerFeedbackFormTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Forms;
+
+use App\Models\CustomerFeedback;
+use Tests\TestCase;
+
+class CustomerFeedbackFormTest extends TestCase
+{
+
+
+  public function test_it_stores_form_data_and_redirects_to_thank_you_page(){
+
+      $test_data = [
+
+          'feedback_score' => 5 ,
+           'feedback_message' => 'Add more service'
+      ];
+
+   $response =  $this->post(route('create-customer-feedback'), $test_data);
+
+   $response->assertRedirect(route('thank-you'));
+
+   $this->assertDatabaseCount(CustomerFeedback::class, 1);
+
+    $this->assertDatabaseHas(CustomerFeedback::class, [
+        'feedback_score' => 5 ,
+        'answer_to_follow_up_question' => 'Add more service'
+    ]);
+
+  }
+
+
+
+}
