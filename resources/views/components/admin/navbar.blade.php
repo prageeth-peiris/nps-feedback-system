c0c207be7f89c3c715c8800b47fa254ce5d3d75a
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-26 20:12:15 +0530

    created admin navbar component

---
+++ resources/views/components/admin/navbar.blade.php
@@ -0,0 +1,13 @@
+<aside class="w-64 bg-gray-800 text-white flex-shrink-0">
+    <div class="p-4">
+        <h1 class="text-2xl font-bold">Admin Panel</h1>
+    </div>
+    <nav class="mt-4">
+        <ul>
+            <li>
+                <a href="/dashboard" class="block px-4 py-2 hover:bg-gray-700 bg-gray-700">Dashboard</a>
+            </li>
+
+        </ul>
+    </nav>
+</aside>
