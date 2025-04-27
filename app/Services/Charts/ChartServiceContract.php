c097f522e3ccc651bf8f645bb5131f8168062209
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 12:32:36 +0530

    created contract for chart service to format data for chart js format

---
+++ app/Services/Charts/ChartServiceContract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\Charts;
+
+use App\DTO\ChartDataDTO;
+
+interface ChartServiceContract
+{
+
+
+    // this must implement the data for bar chart format of chart js
+    public function countOfFeedbacksOfEachResponseGroup() : ChartDataDTO;
+
+
+}
