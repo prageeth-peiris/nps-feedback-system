c2ddd0d8698f1af3145ddbdfddc6452cdc3239ae
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 17:14:56 +0530

    implemented UserRepository contract

---
+++ app/Repositories/User/UserRepositoryImplementation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\DTO\UserCredentialsDTO;
+use App\Models\User;
+
+class UserRepositoryImplementation implements UserRepositoryContract
+{
+    public function retrieveUserByEmail(UserCredentialsDTO $userCredentialsDTO): ?User
+    {
+
+        return  User::where('email', $userCredentialsDTO->email)->first();
+
+
+    }
+
+
+}
