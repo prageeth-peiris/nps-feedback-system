528e54c2155d35b809695f5a62adaf4da7d0ae18
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 19:28:41 +0530

    fixed log in post request name

--- resources/views/frontend/userLogIn.blade.php
+++ resources/views/frontend/userLogIn.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <x-forms.form title="Log In" :action="route('login')" method="POST">
+    <x-forms.form title="Log In" :action="route('attempt-auth')" method="POST">
 
         @error('fail')
         <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
