1bd5cc14750e2edef7bf299dd721a2964a5807bc
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 07:22:23 +0530

    extracted table filter to seperate file

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -1,5 +1,11 @@
 @extends('admin.layout')
 
 @section('content')
-    <x-data-table :records="$records"/>
+    <x-data-table :records="$records">
+
+        <x-slot:filters>
+        @include('components.data-table-filters')
+        </x-slot>
+
+    </x-data-table>
 @endsection
---
+++ resources/views/components/data-table-filters.blade.php
@@ -0,0 +1,38 @@
+<form class="md:flex flex-row flex-wrap justify-between p-3 gap-4" name="filterForm" action="{{route('dashboard')}}" method="GET">
+
+
+<span class="flex gap-4">
+    <x-forms.dropdown
+        label="Response Group"
+        id="response_group"
+        :options="[
+
+        ['Promoter','Promoter'] , ['Detractor','Detractor'] , ['Passive','Passive']
+
+    ]"
+        :required="false"
+
+    />
+
+    <x-forms.dropdown
+        id="limit"
+        label="Record Limit"
+        :options="[
+
+        ['10','10'] , ['25','25'] , ['50','50'] , ['100','100']
+
+    ]"
+        :required="false"
+    />
+</span>
+
+
+    <div class="flex items-end w-1/6 mt-2">
+          <span class="w-full flex gap-2">
+      <x-forms.button label="Apply"/>
+       <x-forms.button type="clear" label="Clear"/>
+    </span>
+    </div>
+
+
+</form>
