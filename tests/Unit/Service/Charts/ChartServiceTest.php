ef2ec7bad88a95848755649a0cd0ff9f142347ba
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 12:38:09 +0530

    created unit test for chart  service to check chart data format

---
+++ tests/Unit/Service/Charts/ChartServiceTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Service\Charts;
+
+use App\DTO\ChartDataDTO;
+use App\DTO\CustomerFeedbackDTO;
+use App\Models\CustomerFeedback;
+use App\Services\Charts\ChartServiceContract;
+use Tests\TestCase;
+
+class ChartServiceTest extends TestCase
+{
+
+
+    public function test_it_returns_chart_data_of_count_of_total_feedback_of_each_response_group()
+    {
+
+        CustomerFeedback::factory()->create(
+
+            (new CustomerFeedbackDTO(feedback_score: 6))->toArray()
+
+        );
+
+        CustomerFeedback::factory()->create(
+
+            (new CustomerFeedbackDTO(feedback_score: 2))->toArray()
+
+        );
+
+        CustomerFeedback::factory()->create(
+
+            (new CustomerFeedbackDTO(feedback_score: 9))->toArray()
+
+        );
+
+        CustomerFeedback::factory()->create(
+
+            (new CustomerFeedbackDTO(feedback_score: 10))->toArray()
+
+        );
+
+        CustomerFeedback::factory()->create(
+
+            (new CustomerFeedbackDTO(feedback_score: 10))->toArray()
+
+        );
+
+
+
+        /** @var ChartDataDTO $data */
+        $data = app(ChartServiceContract::class)->countOfFeedbacksOfEachResponseGroup();
+
+        $this->assertEquals(["Promoter","Detractor"], $data->labels);
+
+        $this->assertEquals([3,2], $data->dataSets);
+
+
+    }
+
+
+
+}
