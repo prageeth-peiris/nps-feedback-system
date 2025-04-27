a5ffe516d0b3ee488d6c01373468c11d60d8c975
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 12:36:05 +0530

    implemented chart service to return ChartData DTO

---
+++ app/Services/Charts/ChartServiceImplementation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services\Charts;
+
+use App\DTO\ChartDataDTO;
+use App\Repositories\CustomerFeedback\CustomerFeedbackRepositoryContract;
+
+class ChartServiceImplementation implements ChartServiceContract
+{
+
+
+    public function __construct(
+       public readonly CustomerFeedbackRepositoryContract $customerFeedbackRepository
+    )
+    {
+    }
+
+    public function countOfFeedbacksOfEachResponseGroup(): ChartDataDTO
+    {
+
+        $data = $this->customerFeedbackRepository->countOfFeedbacksOfEachResponseGroup();
+
+
+
+      return new ChartDataDTO(labels: $data->pluck('response_group')->toArray(),
+          dataSets:$data->pluck('total')->toArray(),labelName: "No of Feedback Count"
+
+      );
+
+    }
+
+
+}
