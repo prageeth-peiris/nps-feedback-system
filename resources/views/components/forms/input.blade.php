4409fadc2df4baa21f3c7896c3147188edf396b9
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 14:02:31 +0530

    if error variable is in flash session then display the message in text input

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,6 +1,8 @@
 <div>
 
-
+    @error($id)
+    <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+    @enderror
     <label for="{{$id}}" class="block text-gray-700 font-medium mb-2">{{$label}}</label>
     <input type="{{$type}}" id="{{$id}}"  {{$required ? 'required' : ''}} name="{{$id}}"
            class="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
