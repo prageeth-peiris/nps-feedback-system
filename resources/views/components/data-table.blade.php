b56280c05e730cc3e841ce5773f6032d65e87616
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-28 00:39:57 +0530

    added csv export button to UI

--- resources/views/components/data-table.blade.php
+++ resources/views/components/data-table.blade.php
@@ -1,7 +1,9 @@
 <div class="bg-white rounded-lg shadow-lg overflow-hidden">
     <!-- Table Header -->
-    <div class="px-6 py-4 border-b border-gray-200">
+    <div class="px-6 py-4 border-b border-gray-200 flex justify-between">
         <h2 class="text-xl font-semibold text-gray-800">All Customer Feedbacks</h2>
+        <a class="w-1/5" href="{{route('export')}}">  <x-forms.button type="reset" label="Export"/></a>
+
     </div>
 
  {{$filters}}
