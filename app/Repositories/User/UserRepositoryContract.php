0370af74dea708bf772e0f99b7f3fe0bc257b458
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 17:14:34 +0530

    created UserRepository contract

---
+++ app/Repositories/User/UserRepositoryContract.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\DTO\UserCredentialsDTO;
+use App\Models\User;
+
+interface  UserRepositoryContract
+{
+
+
+    // find the user by email
+    public function retrieveUserByEmail(UserCredentialsDTO $userCredentialsDTO):?User;
+
+
+
+
+}
