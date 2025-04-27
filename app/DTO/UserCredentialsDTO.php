1b43620b75ddb0f373a881e24b94d3def2da583e
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 17:14:00 +0530

    created data transfer object to hold user credentials data

---
+++ app/DTO/UserCredentialsDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTO;
+
+class UserCredentialsDTO
+{
+
+    public function __construct(
+        public readonly string $email,
+        public readonly string $password,
+    )
+    {
+    }
+}
