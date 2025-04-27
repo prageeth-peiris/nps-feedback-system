a51957cab9fcb75cc24d7e9567b5fa8d4b4ac164
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-28 02:32:42 +0530

    created stat card to display nps score

---
+++ resources/views/components/stat-card.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-white rounded-2xl shadow-md overflow-hidden p-6 flex items-center justify-center">
+    <h2 class="text-xl font-semibold text-gray-800 mb-4">{{$heading }}  : {{$calculateNPSScore()}}</h2>
+
+</div>
