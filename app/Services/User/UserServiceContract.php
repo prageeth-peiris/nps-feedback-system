45ce9b1d3be85c5624cb8d63eed9964263b2248d
Prageeth Peiris <glpspeiris@gmail.com>
2025-04-27 17:19:32 +0530

    created user service contract

---
+++ app/Services/User/UserServiceContract.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\User;
+
+
+
+use App\DTO\UserCredentialsDTO;
+use App\Models\User;
+
+interface UserServiceContract
+{
+
+
+    // indicates whether user credentials matched. if success returns user or throws exceptions
+    public function login(UserCredentialsDTO $userCredentialsDTO):User;
+
+}
